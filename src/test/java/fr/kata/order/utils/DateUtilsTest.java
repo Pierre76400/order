f0783688dc136169720d586aee7ac3588284101b
Bragon76 <pierre.leplay@gmail.com>
2025-03-19 12:06:17 +0100

    fix : Rajout du lanceur pour les TU pour l'utilitaire sur les dates

--- src/test/java/fr/kata/order/utils/DateUtilsTest.java
+++ src/test/java/fr/kata/order/utils/DateUtilsTest.java
@@ -1,13 +1,16 @@
 package fr.kata.order.utils;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class DateUtilsTest {
 
     @Test
-    void stringFormatFrancaisToLocalDate() {
+    public void stringFormatFrancaisToLocalDate() {
         assertEquals("2021-01-01", DateUtils.stringFormatFrancaisToLocalDate("01/01/2021").toString());
     }
 }
